2692c88505d32cc5c5cf5317a71fef39012a4f1f
slama sefi nizar <sln3314b@u1-204-19l.ens.fsi>
2021-09-23 16:42:22 +0200

    Fin du TP

---
+++ TP02/exercice3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ int 
+ crible(int * t){
+ printf("%d\n",t)
+ return(t)
+ }
+int 
+ main(){
+     int tableau(TAILLE)
+     crible(&(tableau[0]));
+     return(0);
+
+     tableau[0]=1
+     tableau[1]=1
+     
---
+++ TP03/tri_tableau.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int 
+somme(int * T, int N) {
+    int res = 0;
+    for ( int i =0; i<N ; i++){
+        res += T[i];
+    }
+    return res;
+}
+int 
+main(){
+    int T1[10];;
+    int s ;
+    for (int i=0; i<10; i++){ 
+        T1[i] = i;
+    }
+s=somme(T1,10);
+printf( "la somme des  nombres  jusqu ’a 9  est %d\n",s);
+return 0;
+}
