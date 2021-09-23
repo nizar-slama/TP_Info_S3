c1955becb71d129c13da7bfe02dc70e64a97c8ce
nizar-slama <90394230+nizar-slama@users.noreply.github.com>
2021-09-23 12:41:10 +0200

    Create sizeof.c

---
+++ TP02/sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("%lu\n", sizeof(char));
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(short int));
+    printf("%lu\n", sizeof(long int));
+    printf("%lu\n", sizeof(float));
+    printf("%lu\n", sizeof(double));
+    printf("%lu\n", sizeof(int *));
+    printf("%lu\n", sizeof(char *));
+   ;
+    return 0;
