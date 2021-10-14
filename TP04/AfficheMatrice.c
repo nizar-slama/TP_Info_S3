6cf06a32d9567c49e4607456833b4ab1fa7a921e
slama sefi nizar <sln3314b@u1-204-19l.ens.fsi>
2021-10-14 14:04:12 +0200

    Fin du TP

--- TP04/AfficheMatrice.c
+++ TP04/AfficheMatrice.c
@@ -16,11 +16,11 @@ void AfficheMatrice (float **M, int n, int m) {
 
 int main() {
     float **M=CreerMatrice(2,2);
-     M[0][0]=1
-    M[0][1]=2
-     M[1][0]=3
-      M[1][1]=1
-AfficheMatrice(M,2,2)
-return 0:
+     M[0][0]=1;
+    M[0][1]=2;
+     M[1][0]=3;
+      M[1][1]=1;
+AfficheMatrice(M,2,2);
+return 0;
 }
 #endif
--- TP04/CreerMatrice.c
+++ TP04/CreerMatrice.c
@@ -14,10 +14,10 @@ return res;
 #include <stdio.h>
  int main() {
      float **M= CreerMatrice(2,2);
-     M[0][0]=1
-     M[0][1]=2
-     M[1][0]=3
-    M[1][1]=4
+     M[0][0]=1;
+     M[0][1]=2;
+     M[1][0]=3;
+    M[1][1]=4;
      for (int i=0; i<2; i++){
          printf("( ");
          for ( int j=0 ; j<2 ; j++){
